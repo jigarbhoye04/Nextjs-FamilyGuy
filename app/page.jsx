71630996b4f1e5281e1662aea5294f115cdec06c
jigx.04 <84662430+jigarbhoye04@users.noreply.github.com>
2024-10-13 15:09:37 +0530

    API Routing

--- app/page.jsx
+++ app/page.jsx
@@ -7,13 +7,45 @@ Renders a Next.js page component that displays a grid of character avatars with 
 
 import { Container } from '@/components'
 import Image from 'next/image'
-import Link from 'next/link'
+import Link from 'next/link'  
 import { endpoint } from '@/utils/endpoint'
 
+// async function getAllCharacters() {
+//   const data = await fetch(`${endpoint}/characters`)
+
+//   if (!data.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
+
+//   return data.json()
+// }
+
+import { getAllCharacters } from '@/lib/characters'
+
 export default async function Page() {
+  const data = await getAllCharacters()
+
   return (
-    <main className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl">hello from boilerplate!</h1>
+    <main>
+      <Container className="grid grid-cols-2 gap-1 py-5 md:grid-cols-3 lg:grid-cols-4">
+        {data?.characters?.map(item => {
+          return (
+            <Link
+              href={`/characters/${item.slug}`}
+              key={item.name}
+              className="overflow-hidden rounded-md"
+            >
+              <Image
+                src={item.avatar}
+                alt=""
+                className="transition-all duration-500 hover:scale-110 hover:-rotate-2"
+                width={500}
+                height={500}
+              />
+            </Link>
+          )
+        })}
+      </Container>
     </main>
   )
 }
