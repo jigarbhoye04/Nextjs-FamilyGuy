6c056d7a874f4bf195653af5e9e738b1a5f1847c
jigx.04 <84662430+jigarbhoye04@users.noreply.github.com>
2024-10-13 15:35:11 +0530

    Metadata UPD.

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,3 @@
-/**
- * Root layout component for the Family Guy app.
- *
- * @component
- * @param {Object} props - The component props.
- * @param {ReactNode} props.children - The child components.
- * @returns {JSX.Element} The rendered root layout.
- */
-
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Navigation } from '@/components'
@@ -16,11 +7,15 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Family Guy',
   description: 'NEXTJS LEARNING!',
+  icon: '/StewieGriffin.webp',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href={metadata.icon} />
+      </head>
       <body>
         <Navigation />
         {children}
@@ -28,3 +23,19 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
+
+
+
+
+/**
+ * Root layout component for the Family Guy app.
+ *
+ * @component
+ * @param {Object} props - The component props.
+ * @param {ReactNode} props.children - The child components.
+ * @returns {JSX.Element} The rendered root layout.
+ */
+
+
+
+
